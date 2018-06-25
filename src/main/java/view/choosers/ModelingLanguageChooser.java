c60797bcf936c9203a617e5f2a6b9088949ff5ae
Moritz März <moritz.maerz@stud.uni-bamberg.de>
2018-06-25 16:43:27 +0200

    ModelingLanguageChooser fertig

--- src/main/java/view/choosers/ModelingLanguageChooser.java
+++ src/main/java/view/choosers/ModelingLanguageChooser.java
@@ -2,32 +2,79 @@ package view.choosers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.ontology.ModelingOntology;
 import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.search.EntitySearcher;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Set;
 
 public final class ModelingLanguageChooser {
 
     public static void showModelingLanguageChooser(){
+        OWLOntology ontology = ModelingOntology.getInstance().getOntology();
+        String ontID = ontology.getOntologyID().getOntologyIRI().asSet().toString().replace("[", "").replace("]", "");
+        String type = "";
+
+        Map<String, OWLNamedIndividual> individualStore = new HashMap<>();
+
         LinkedList<String> modelType = new LinkedList<>();
-        Set<OWLNamedIndividual> individuals =  ModelingOntology.getInstance().getOntology().getIndividualsInSignature();
+        Set<OWLNamedIndividual> individuals = ontology.getIndividualsInSignature();
 
         for (OWLNamedIndividual individual: individuals) {
-            if (individual.getEntityType().toString().equals("Modeltype")){
-                modelType.add(individual.getEntityType().toString());
+            if (EntitySearcher.getTypes(individual, ontology).toString()
+                    .replace("[", "").replace("]", "").replace("<", "").replace(">", "")
+                    .equals(ontID + "#Modeltype")){
+                String individualName = individual.toString().replace("<", "").replace(">", "");
+                String[] splitArray = individualName.split("#");
+                type = splitArray[splitArray.length-1];
+                modelType.add(type);
+
+                individualStore.put(type, individual);
             }
         }
-        final ObservableList<String> modelTypeList = FXCollections.observableList(modelType);
-        final ListView<String> listView = new ListView<>(modelTypeList);
+        ObservableList<String> modelTypeList = FXCollections.observableList(modelType);
+        ListView<String> listView = new ListView<>(modelTypeList);
+
+        VBox vBox = new VBox(10);
+        vBox.setAlignment(Pos.CENTER);
+
+        HBox buttons = new HBox(10);
+        buttons.setAlignment(Pos.CENTER);
+        Button accept = new Button("accept");
+        Button cancel = new Button("cancel");
+        buttons.getChildren().addAll(accept, cancel);
+
+        Label space = new Label("");
+
+        vBox.getChildren().addAll(listView, buttons, space);
 
         Stage primaryStage = new Stage();
-        primaryStage.setTitle("Choose modeling language: ");
-        primaryStage.setScene(new Scene(listView));
-        primaryStage.show();
+        primaryStage.setTitle("Choose modelinglanguage:");
+        primaryStage.setScene(new Scene(vBox));
+
+        // Eventhandler
+        String finalType = type;
+        accept.setOnAction(event -> {
+            primaryStage.close();
+            //TODO: Toolbar mit passender Sprache laden
+            System.out.println(individualStore.get(listView.getSelectionModel().getSelectedItem()));
+        });
+        cancel.setOnAction(event -> primaryStage.close());
+
+
+
+        primaryStage.showAndWait();
     }
 }
