b2e7681e1a052632c8b85d52be85327a28c6bfd3
Moritz März <moritz.maerz@stud.uni-bamberg.de>
2018-07-13 09:54:27 +0200

    OntologyTreeBuilder finished

--- src/main/java/controller/ontology/OntologyTreeBuilder.java
+++ src/main/java/controller/ontology/OntologyTreeBuilder.java
@@ -7,45 +7,39 @@ import javafx.scene.image.ImageView;
 import model.singleton.OWLEditorKitManager;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
-import view.jfxviews.CreateElement;
 
 public class OntologyTreeBuilder {
-    public static void buildTreeView(OWLOntology ontology, TreeView<String> treeView){
-        Image classIcon = new Image(CreateElement.class.getResourceAsStream("/classIcon.gif"));
+    public static void buildTreeView(OWLOntology ontology, TreeView<String> treeView, Image icon){
 
         // Childs of RootItem
         for (OWLClass owlClass: ontology.getClassesInSignature()){
             for (OWLClass parent : OWLEditorKitManager.getInstance().getEditorKit().getOWLModelManager().getOWLHierarchyManager().getOWLClassHierarchyProvider().getParents(owlClass)) {
                 if (parent.isOWLThing()){
-                    TreeItem<String> child = new TreeItem(OntologyStringBuilder.getEntityFromOWLClass(ontology, owlClass), new ImageView(classIcon));
+                    TreeItem<String> child = new TreeItem(OntologyStringBuilder.getEntityFromOWLClass(ontology, owlClass), new ImageView(icon));
                     treeView.getRoot().getChildren().add(child);
                 }
             }
         }
 
         // All non childs of root Item
-        for (TreeItem<String> classItem: treeView.getRoot().getChildren()){
-            addChildrenToParents(ontology, classItem, classIcon, treeView);
+        for (TreeItem<String> item: treeView.getRoot().getChildren()){
+            addChildrenToParents(ontology, item, icon, treeView);
         }
 
         expandTreeView(treeView.getRoot(), false);
         treeView.getRoot().setExpanded(true);
     }
 
-    private static void addChildrenToParents(OWLOntology ontology, TreeItem<String> rootItem, Image classIcon, TreeView<String> treeView) {
+    private static void addChildrenToParents(OWLOntology ontology, TreeItem<String> rootItem, Image icon, TreeView<String> treeView) {
         for (OWLClass owlClass: ontology.getClassesInSignature()){
             for (OWLClass parent : OWLEditorKitManager.getInstance().getEditorKit().getOWLModelManager().getOWLHierarchyManager().getOWLClassHierarchyProvider().getParents(owlClass)) {
                 if (OntologyStringBuilder.getEntityFromOWLClass(ontology, parent).equals(rootItem.getValue())){
-                    TreeItem<String> child = new TreeItem(OntologyStringBuilder.getEntityFromOWLClass(ontology, owlClass), new ImageView(classIcon));
-                    System.out.println("Child: " + child);
+                    TreeItem<String> child = new TreeItem(OntologyStringBuilder.getEntityFromOWLClass(ontology, owlClass), new ImageView(icon));
                     int rowOfParent = treeView.getRow(rootItem);
-                    System.out.println("RowOfParents: " + rowOfParent);
-                    //treeView.getTreeItem(rowOfParent) fehler bei rowOfParent=6
                     TreeItem<String> parentTreeItem = treeView.getTreeItem(rowOfParent);
-                    System.out.println("ParentTreeItem: " + parentTreeItem);
                     parentTreeItem.getChildren().add(child);
                     parentTreeItem.setExpanded(true);
-                    addChildrenToParents(ontology, child, classIcon, treeView);
+                    addChildrenToParents(ontology, child, icon, treeView);
 
                 }
             }
